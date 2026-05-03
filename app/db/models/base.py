c8895b6c19421928f9655032881cdcff26e45949
Anna Veselova <anna.veselova.97@mail.ru>
2026-05-03 22:09:52 +0300

    add camel_to_snake to tablenames

--- app/db/models/base.py
+++ app/db/models/base.py
@@ -1,7 +1,13 @@
+import re
+
 from sqlalchemy.orm import DeclarativeBase, declared_attr
+
+
+def camel_to_snake(name: str) -> str:
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
 
 
 class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+        return camel_to_snake(cls.__name__)
